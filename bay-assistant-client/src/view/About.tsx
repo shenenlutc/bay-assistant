1c6e34974c43215fcdc5bd5f666104dfa316b2d9
enlu shen <enlu.shen.ext@bayer.com>
2024-03-20 22:03:27 +0800

    update

--- bay-assistant-client/src/view/About.tsx
+++ bay-assistant-client/src/view/About.tsx
@@ -1,7 +1,20 @@
+const aboutDivStyle : React.CSSProperties = {
+    fontSize: "xx-large",
+    position: "relative",
+    height: "300px",
+    top: "10%",
+    border: "solid 1px",
+    textAlign: "center",
+    alignItems:"center"
+}
+
+
 const About: React.FC = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div style={aboutDivStyle}>
+            <p>拜助理</p>
+            <p>版本 3.16.4</p>
+            <p >拜耳中国版权所有</p>
         </div>
     )
 };
