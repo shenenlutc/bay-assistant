74670107dc4b4472e1843a7274dda0d71898b9e8
Enlu Shen <enlu.shen.ext@bayer.com>
2024-03-28 13:42:31 +0800

    update
    update

--- bay-assistant-client/src/view/Application.tsx
+++ bay-assistant-client/src/view/Application.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect, useRef, useContext } from "react";
-import { useParams, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Col, Row, Button } from "antd";
 import { List, ListRowRenderer, AutoSizer } from "react-virtualized";
 import "../assets/style/applicant.scss";
@@ -18,7 +18,6 @@ const Application: React.FC = () => {
   const { t, i18n } = useTranslation(); //语言切换
   const [data, setData] = useState([]); // 用于存储获取的数据
   const myListRef = useRef<List>(null);
-  const [filenetData, setFilenetData] = useState([]); // 用于存储获取的filenet数据
   const { value } = useContext(MyContext); //头部搜索框传递过来的值
   const prevValue = useRef(value);
   //更多按钮
@@ -63,15 +62,6 @@ const Application: React.FC = () => {
 
   useEffect(() => {
     getList(); //获取所有数据的方法
-    //获取的filenet数据
-    axios
-      .get("/api/filenet/list")
-      .then((response) => {
-        setFilenetData(response.data); // 设置数据状态
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error); // 错误处理
-      });
   }, []);
 
   // {A:[{...},{..,}], B:[{},{}]} 渲染右侧索引的数据格式：["A","B","D"]
